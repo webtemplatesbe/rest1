6fc1881ea5f9ce4ad3cd489d147ad35d3dd974a4
Wouter Vandenbossche <woutervandenbossche@Wouters-MacBook-Pro.local>
2025-04-19 00:31:57 +0200

    fixing run dev

---
+++ restaurant1/next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true
+  }
+};
+
+module.exports = nextConfig;
